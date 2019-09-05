9557d114a3227ace3effaaf66eb479f33f6dbcf6
Lu <sblintw@gmail.com>
2019-09-05 16:10:09 +0800

    modify search function args

--- insumon/app/src/main/java/com/example/insumon/Search.java
+++ insumon/app/src/main/java/com/example/insumon/Search.java
@@ -16,12 +16,12 @@ public class Search {
         return dataBase;
     }
 
-    public int searchBloodSugarBeforeDish( String dateTime, int time){
+    public int searchBloodSugarBeforeDish( String dateTime, String time){
         int foundSugar = 0;
         search = new Search();
         dataBase = search.getDataBase();
         for(int i = 0; i < dataBase.size(); i++){
-            if((((String)dataBase.get(i)[0]).equals(dateTime)) && ((int)dataBase.get(i)[2] == 0) && ((int)dataBase.get(i)[3] == time)){
+            if((((String)dataBase.get(i)[0]).equals(dateTime)) && (((String)dataBase.get(i)[2]).equals("0")) && (((String)dataBase.get(i)[3]).equals(time))){
                 foundSugar = (int)dataBase.get(i)[1];
                 break;
             }
@@ -42,12 +42,12 @@ public class Search {
         return foundSugar;
     }
 
-    public int searchEatTime( String dateTime, String sugar, int time){
+    public int searchEatTime( String dateTime, String sugar, String time){
         int foundEatTime = 0;
         search = new Search();
         dataBase = search.getDataBase();
         for(int i = 0; i < dataBase.size(); i++){
-            if((((String)dataBase.get(i)[0]).equals(dateTime)) && (((String)dataBase.get(i)[1]).equals(sugar)) && ((int)dataBase.get(i)[3] == time)){
+            if((((String)dataBase.get(i)[0]).equals(dateTime)) && (((String)dataBase.get(i)[1]).equals(sugar)) && (((String)dataBase.get(i)[3]).equals(time))){
                 foundEatTime = (int)dataBase.get(i)[2];
                 break;
             }
@@ -55,12 +55,12 @@ public class Search {
         return foundEatTime;
     }
 
-    public int searchtime( String dateTime, String sugar, int eatTime){
+    public int searchtime( String dateTime, String sugar, String eatTime){
         int foundTime = 0;
         search = new Search();
         dataBase = search.getDataBase();
         for(int i = 0; i < dataBase.size(); i++){
-            if((((String)dataBase.get(i)[0]).equals(dateTime)) && (((String)dataBase.get(i)[1]).equals(sugar)) && ((int)dataBase.get(i)[2] == eatTime)){
+            if((((String)dataBase.get(i)[0]).equals(dateTime)) && (((String)dataBase.get(i)[1]).equals(sugar)) && (((String)dataBase.get(i)[2]).equals(eatTime))){
                 foundTime = (int)dataBase.get(i)[3];
                 break;
             }
@@ -68,12 +68,12 @@ public class Search {
         return foundTime;
     }
 
-    public static String searchDateTime(String sugar, int eatTime, int time){
+    public static String searchDateTime(String sugar, String eatTime, String time){
         String foundDateTime = null;
         search = new Search();
         dataBase = search.getDataBase();
         for(int i = 0; i < dataBase.size(); i++){
-            if((((String)dataBase.get(i)[1]).equals(sugar)) && ((int)dataBase.get(i)[2] == eatTime) && ((int)dataBase.get(i)[3] == time)){
+            if((((String)dataBase.get(i)[1]).equals(sugar)) && (((String)dataBase.get(i)[2]).equals(eatTime)) && (((String)dataBase.get(i)[3]).equals(time))){
                 foundDateTime = (String)(dataBase.get(i)[0]);
                 break;
             }
